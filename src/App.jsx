63a3aa252acee16ba88205cf449f30bdb4f180ba
hellokev <2kevinloi8@gmail.com>
2023-09-21 20:01:38 -0700

    template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import CreateForm from './components/CreateForm.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="form-container">
+        <CreateForm />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CreateForm.jsx
@@ -0,0 +1,5 @@
+export default function CreateForm() {
+    return (<h1>
+            Test
+        </h1>)
+}
